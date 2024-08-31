8badbc1bff5a95c1a3707dc785f9507f459fb68d
elliot-hacks <thisguyhack@gmail.com>
2024-09-01 01:30:30 +0300

    Center the Calender

--- home/urls.py
+++ home/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('calendar/', views.CalendarView.as_view(), name='calendar'),
     path('event/new/', views.event, name='event_new'),
-    path('event/edit/(?P<event_id>\d+)/$', views.event, name='event_edit'),
+    path('event/edit/<int:event_id>/', views.event, name='event_edit'),
 ]
